d43ed3e2607fa2e4b08b5ca1410671d28306d0a1
Dadou Wicket <sbar54@hotmail.fr>
2026-06-19 10:02:57 +0200

    feat(auth): add API login request with fetch

--- FrontEnd/api/auth.js
+++ FrontEnd/api/auth.js
@@ -1,26 +1,37 @@
 import { BASE_API } from "../utils/const.js";
 
+// Envoie email + password à l'API et récupère un token si succès
 export async function login(email, password) {
   try {
-    const response = await fetch(BASE_API + "/login", {
+    // Envoi de la requête POST vers l'API login
+    const response = await fetch(BASE_API + "/users/login", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ email, password }),
     });
 
+    // Si la réponse du serveur est OK
     if (response.ok) {
+      // on récupère les données renvoyées par l'API (token)
       let data = await response.json();
+
+      // On stocke le token dans le localStorage pour garder l'utilisateur connecté
       localStorage.setItem("token", data.token);
+
       return true;
     } else {
-      //   alert("Mot de passe ou email introuvable");
       return false;
     }
-  } catch (e) {
+  } catch (error) {
+    // Si erreur réseau ou serveur inaccessible
     return false;
   }
 }
 
 export async function logout() {
+  // On supprime le token pour déconnecter l'utilisateur
   localStorage.removeItem("token");
 }
 
@@ -34,22 +45,33 @@ export function isLogged() {
 
 const form = document.querySelector("#login-form");
 
-function handleLogin(e) {
-  e.preventDefault();
+//Fonction de soumission du formulaite
+async function submitLoginForm(event) {
+  event.preventDefault(); // empêche le rechargement de la page
 
+  // Récupération des valeurs du formulaire
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
 
-  login(email, password).then(function (success) {
-    if (success) {
-      window.location.href = "index.html";
-    } else {
-      document.querySelector(".error").textContent =
-        "Email ou mot de passe incorrect";
-    }
-  });
+  // Appel de la fonction login
+  const success = await login(email, password);
+
+  // Si connexion réussie → redirection
+  if (success) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  // Sinon → affichage du message d’erreur
+  const errorMsg = document.querySelector(".error");
+
+  if (errorMsg) {
+    errorMsg.textContent = "Erreur dans l’identifiant ou le mot de passe";
+  }
 }
 
+// Ajout de l'écouteur d'événement sur le formulaire, quand l'utilisateur
+// clique sur "Envoyer", appelle la fonction submitLoginForm
 if (form) {
-  form.addEventListener("submit", handleLogin);
+  form.addEventListener("submit", submitLoginForm);
 }
