8d56df328dbc596a5a63ebd6ed19931459803015
Dadou Wicket <sbar54@hotmail.fr>
2026-06-29 10:36:06 +0200

    git commit -m "Add project gallery modal"

--- FrontEnd/controllers/homepage.js
+++ FrontEnd/controllers/homepage.js
@@ -161,10 +161,3 @@ const closeBtn = document.querySelector(".modal-close");
 closeBtn.addEventListener("click", function () {
   modal.style.display = "none";
 });
-
-//Fermeture de la modal en cliquant à l'exterieur
-modal.addEventListener("click", function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-});
